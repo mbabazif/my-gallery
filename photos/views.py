b72ef772476245408e04842813fee84aa548a37a
Florence <mbababziflorence81@gmail.com>
2019-03-15 11:28:58 +0200

    adding the function  past_days_photos in my project

--- photos/views.py
+++ photos/views.py
@@ -10,6 +10,8 @@ def welcome(request):
 
 def photos_of_day(request):
     date = dt.date.today()
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
@@ -17,5 +19,29 @@ def photos_of_day(request):
             </body>
         </html>
             '''
-    return HttpResponse(html)    
+    return HttpResponse(html) 
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+def past_days_photos(request,past_date):
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+        day = convert_dates(date)
+        html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+        return HttpResponse(html)    
 
