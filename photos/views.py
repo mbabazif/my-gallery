740e1633d8e052c215d16de1ff65b2d4ff3b8f56
Florence <mbababziflorence81@gmail.com>
2019-03-15 12:41:48 +0200

    adding the date to project

--- photos/views.py
+++ photos/views.py
@@ -1,47 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
-from django.http  import HttpResponse
-
-# Create your views here.
 def welcome(request):
-    return HttpResponse('Welcome to My Gallery')
+    
+    return render(request, 'welcome.html')
+    # return HttpResponse('Welcome to My Gallery')
 
 def photos_of_day(request):
     date = dt.date.today()
+    return render(request, 'all-photos/today-photos.html', {"date": date,})
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1> {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html) 
+    # day = convert_dates(date)
+    # html = f'''
+    #     <html>
+    #         <body>
+    #             <h1> {date.day}-{date.month}-{date.year}</h1>
+    #         </body>
+    #     </html>
+    #         '''
+    # return HttpResponse(html) 
 
-def convert_dates(dates):
+# def convert_dates(dates):
 
-    # Function that gets the weekday number for the date.
-    day_number = dt.date.weekday(dates)
+#     # Function that gets the weekday number for the date.
+#     day_number = dt.date.weekday(dates)
 
-    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
 
-    # Returning the actual day of the week
-    day = days[day_number]
-    return day
+#     # Returning the actual day of the week
+#     day = days[day_number]
+#     return day
+    # View Function to present news from past days
 def past_days_photos(request,past_date):
+    try:
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
-        day = convert_dates(date)
-        html = f'''
-        <html>
-            <body>
-                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-        return HttpResponse(html)    
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+    if date == dt.date.today():
+        return redirect(photos_of_day)
+
+    return render(request, 'all-photos/past-photos.html', {"date": date})
+
+        # day = convert_dates(date)
+        # html = f'''
+        # <html>
+        #     <body>
+        #         <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+        #     </body>
+        # </html>
+        #     '''
+        # return HttpResponse(html)    
 
