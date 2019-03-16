70429865fe204a70a90a5c736b2ac48707c1c606
Florence <mbababziflorence81@gmail.com>
2019-03-16 10:37:30 +0200

    adding the method to delete image in the database

--- gallery/settings.py
+++ gallery/settings.py
@@ -119,8 +119,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 # Static files (CSS, JavaScript, Images)
@@ -129,4 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 ]
