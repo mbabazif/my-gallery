340f0ef2a28dbee2e75d0358221bb1b1bc3896c8
Florence <mbababziflorence81@gmail.com>
2019-03-20 10:05:52 +0200

    adding the link for the single image

--- photos/tests.py
+++ photos/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Location,Category,Images
 
 # Create your tests here.
+class ImagesTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+    
+    Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance()
+
+    Testing Save Method
+    def test_save_method(self):
+        self.save_Images()
+        images = Images.objects.all()
+        self.assertTrue(len(emages) > 0)
+
+    
